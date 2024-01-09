309e43a67bbed1b13ae726bca79766294433f98c
Louis0615s <130944205+Louis0615s@users.noreply.github.com>
2024-01-09 15:52:23 +0800

    Add files via upload

--- Lightdance-TLC/include/OFController.h
+++ Lightdance-TLC/include/OFController.h
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <vector>
 
-// library for PCA
+// library for TLC59116F
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
@@ -36,8 +36,9 @@ class OFController {
         int sendAll(const vector<int> &statusLists);
     private:
         int I2CInit();
-        int fd[Config::NUMPCA];
+        int fd[Config::NUMTLC];
         bool err_flag[Config::NUMPCA];
         void I2C_Specified_Init(int i);
 };
+
 #endif
--- Lightdance-TLC/src/OFController.cpp
+++ Lightdance-TLC/src/OFController.cpp
@@ -27,6 +27,7 @@ void OFColor::setColor(const int &colorCode) {
     r = int(r_cal);
     g = int(g_cal);
     b = int(b_cal);
+
 }
 
 int OFColor::getR() { return r; }
@@ -39,14 +40,15 @@ int OFController::init() {
     printf("I2C Hardware Initialzed\n");
 
     // open I2C bus
-    for (int i = 0; i < Config::NUMPCA; i++) {
-        err_flag[i] = true;  
-        if((fd[i] = I2CInit()) < 0) {
+    for (int i = 0; i < Config::NUMTLC; i++) {
+        err_flag[i] = true; 
+        if ((fd[i] = I2CInit()) < 0) {
             fprintf(stderr, "I2C of %d init fail.\n", i);
             return 1;
         }
-        
-        if(ioctl(fd[i], I2C_SLAVE, Config::PCAAddr[i]) < 0) {
+        // printf("File descriptor of %d opened at %d.\n", i, fd[i]);
+
+        if (ioctl(fd[i], I2C_SLAVE, Config::TLCAddr[i]) < 0) {
             fprintf(stderr, "Failed to acquire bus access and/or talk to slave %d", i);
             return 2;
         }
@@ -56,13 +58,13 @@ int OFController::init() {
 
 int OFController::sendAll(const vector<int> &statusLists) {
     unsigned char buffer[16];
-    buffer[0] = 0x88;  //Auto increment PWMx
+    buffer[0] = 0xA2;  //Auto increment PWMx
     int counter;
     OFColor Status;
-    for(int i = 0; i < Config::NUMPCA; i++) {
+    for (int i = 0; i < Config::NUMTLC; i++) {
         counter = 0;
         if(err_flag[i] == false) {
-            for(int j = i * 5; j < 5 * (i + 1); j++) {
+            for (int j = i * 5; j < 5 * (i + 1); j++) {
                 Status.setColor(statusLists[j]);
                 buffer[counter * 3 + 1] = Status.getR();
                 buffer[counter * 3 + 2] = Status.getG();
@@ -70,7 +72,7 @@ int OFController::sendAll(const vector<int> &statusLists) {
                 counter++;
             }
             if(write(fd[i], buffer, 16) != 16) {
-                fprintf(stderr, "Failed to write to the I2C bus %x.\n", Config::PCAAddr[i]);
+                fprintf(stderr, "Failed to write to the I2C bus %x.\n", Config::TLCAddr[i]);
                 err_flag[i] = true;
             }
         }
@@ -91,6 +93,7 @@ int OFController::sendAll(const vector<int> &statusLists) {
             }
         }
     }
+
     return 1;
 }
 
@@ -108,11 +111,26 @@ void OFController::I2C_Specified_Init(int i) {
     if (ioctl(fd[i], I2C_SLAVE, Config::PCAAddr[i]) < 0) {
         fprintf(stderr, "Failed to acquire bus access and/or talk to slave %d", i);
     }
-    unsigned char buffer[2];
-    buffer[0] = 0x45;
-    buffer[1] = 0xFF;
-    if(write(fd[i], buffer, 2) != 2) {
-        fprintf(stderr, "failed to reconnect to I2C bus %x.\n", Config::PCAAddr[i]);
+    unsigned char buffer[25];
+    //AUTO_INCREMENT_ALL_REGISTERS
+    buffer[0] = 0x80;           
+    //Mode 1
+    buffer[1] = 0xA0;
+    //Mode 2
+    buffer[2] = 0x00;
+    //set PWM0-PWM15 to 0
+    for(int i = 0; i < 16; i++) 
+        buffer[i+3] = 0x00;
+    //GRPPWM
+    buffer[19] = 0x00;
+    //GRPFREQ
+    buffer[20] = 0x00;
+    //LEDOUT setup
+    for(int i = 0; i < 4; i++)
+        buffer[i+21] = 0xAA;
+    //Write to TLC59116F
+    if (write(fd[i], buffer, 25) != 25) {
+        fprintf(stderr, "Failed to write to the I2C bus %x.\n", Config::TLCAddr[i]);
     }
     else {
         err_flag[i] = false;
